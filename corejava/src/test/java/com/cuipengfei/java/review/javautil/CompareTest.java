48f6048fce101d06da6fedd27b676aeddfbdc7d7
cuipengfei <cuipf1007>
2017-07-19 17:47:49 +0800

    单元测试目录

---
+++ corejava/src/test/java/com/cuipengfei/java/review/javautil/CompareTest.java
@@ -0,0 +1,16 @@
+package com.cuipengfei.java.review.javautil;
+
+import org.junit.Test;
+
+/**
+ * Created by cuipengfei on 17-7-19.
+ */
+public class CompareTest {
+
+
+    @Test
+    public void test(){
+        CompareDemo compareDemo=new CompareDemo();
+        System.out.println(compareDemo.toString());
+    }
+}
